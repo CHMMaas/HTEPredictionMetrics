d7d900e3adc7a454b46c688e5ff4b79f99059d74
Carolien Maas <c.h.m.maas@erasmusmc.nl>
2022-02-24 18:52:27 +0100

    Edits val_surv_mi

--- R/val_surv_mi.R
+++ R/val_surv_mi.R
@@ -1,12 +1,14 @@
 #' @title val_surv_mi
 #' @description This function calculates intercept, slope, and C-index for risk predictions of multiple imputed data set(s).
 #'
-#' @importFrom rms Predict cph
+#' @importFrom rms Predict
+#' @importFrom rms cph
+#' @importFrom rms rcs
 #' @importFrom survival coxph
+#' @importFrom survival survfit
 #' @importFrom Hmisc rcorr.cens
-#' @importFrom grDevices dev.new
 #' @importFrom grDevices dev.off
-#' @importFrom grDevices savePlot
+#' @importFrom grDevices png
 #' @importFrom graphics abline
 #' @importFrom graphics legend
 #' @importFrom graphics lines
@@ -22,16 +24,19 @@
 #' @param y Time to event outcome as Surv object (time,status)
 #' @param g Number of risk groups; default=5
 #' @param main Plot label, default=""
+#' @param file_path directory to save figures to, only used if save_plots=TRUE, default=""
 #' @param save_plots Save plots to file, default=FALSE
 #' @param time Time point at which to evaluate the predicted probabilities, default=NULL (not entered), the maximum time point will be taken
 #' @param lim limit, default=NULL
 #' @param dist distribution, default=TRUE
+#' @param CI plot confidence interval, default=FALSE
+#' @param df degrees of freedom to compute confidence interval, default=3
 #'
 #' @return The output of the val_surv_mi function is a "list" with the following components.
 #'
 #' main
 #'
-#' Plot label
+#' Main title of plots.
 #'
 #'
 #' n
@@ -46,17 +51,22 @@
 #'
 #' p.mi
 #'
-#' predicted probabilities for each imputed data set.
+#' predicted survival for each imputed data set.
 #'
 #'
 #' obs.mi
 #'
-#' observed probabilities lower bound of 95% confidence interval.
+#' observed survival for each imputed data set.
 #'
 #'
 #' obs.mi.lower
 #'
-#' observed probabilities lower bound of 95% confidence interval.
+#' lower bound of 95% confidence interval of observed survival.
+#'
+#'
+#' obs.mi.upper
+#'
+#' upper bound of 95% confidence interval of observed survival.
 #'
 #'
 #' int
@@ -66,12 +76,12 @@
 #'
 #' int.lower
 #'
-#' intercept lower bound of 95% confidence interval.
+#' lower bound of 95% confidence interval of intercept.
 #'
 #'
 #' int.upper
 #'
-#' intercept upper bound of 95% confidence interval.
+#' upper bound of 95% confidence interval of intercept.
 #'
 #'
 #' slope
@@ -81,24 +91,27 @@
 #'
 #' slope.lower
 #'
-#' slope estimate lower bound of 95% confidence terval.
+#' lower bound of 95% confidence interval of slope.
 #'
 #'
 #' slope.upper
 #'
-#' slope estimate upper bound of 95% confidence terval.
+#' upper bound of 95% confidence interval of slope.
+#'
 #'
 #' cindex
 #'
 #' C-index
 #'
+#'
 #' cindex.lower
 #'
-#' C-index lower bound of 95% confidence terval.
+#' lower bound of 95% confidence interval of C-index.
 #'
 #' cindex.upper
 #'
-#' C-index upper bound of 95% confidence terval.
+#' upper bound of 95% confidence interval of C-index.
+#'
 #' @export
 #'
 #' @examples
@@ -115,7 +128,8 @@
 #' save_plots <- FALSE
 #' time <- 30
 #' val.surv.mi(p=p,y=y,g=g,main=main,save_plots=save_plots,time=time)
-val.surv.mi<-function(p,y,g=5,main="",save_plots=FALSE,time=NULL,lim=c(0,1),dist=TRUE){
+val.surv.mi<-function(p,y,g=5,main="",file_path="",save_plots=FALSE,time=NULL,
+                      lim=c(0,1),dist=TRUE,CI=FALSE, df=3){
   if (!is.null(time)){
     y[y[,1]>time,2]<-0
     y[y[,1]>time,1]<-time
@@ -146,14 +160,13 @@ val.surv.mi<-function(p,y,g=5,main="",save_plots=FALSE,time=NULL,lim=c(0,1),dist
 
     f.val<-survival::coxph(y~lp.val)
     f.val.offset<-survival::coxph(y~offset(lp.val))
-    f.val.rcs<-rms::cph(y~rms::rcs(lp.val,5),x=TRUE,y=TRUE)
+    f.val.rcs<-rms::cph(y~rms::rcs(lp.val,df),x=TRUE,y=TRUE) # TODO: make df adjustable
 
-    surv.sm<-rms::Predict(f.val.rcs,lp.val=lp.range,conf.int = 0.95, conf.type = "simultaneous",time=max(y[,1]))
+    surv.sm<-Predict(f.val.rcs,lp.val=lp.range,conf.int = 0.95, conf.type = "simultaneous",time=max(y[,1]))
     lp.sm[,i]<-log(-log(surv.sm$yhat))
     lp.sm.se[,i]<-(log(-log(surv.sm$upper))-log(-log(surv.sm$lower)))/(stats::qnorm(.975)-stats::qnorm(.025))
-    #cindex[i]<-f.val$stats["C"]
 
-    rc<-rcorr.cens(-lp.val,y)
+    rc<-Hmisc::rcorr.cens(-lp.val,y)
     cindex[i]<-rc["C Index"]
     cindex.se[i]<-rc["S.D."]/2
 
@@ -164,7 +177,7 @@ val.surv.mi<-function(p,y,g=5,main="",save_plots=FALSE,time=NULL,lim=c(0,1),dist
     log.H<-log(-log(utils::tail(sf$surv,1)))
     log.H.upper<-log(-log(utils::tail(sf$upper,1)))
     int[i]<-log.H-mean(f.val.offset$linear.predictors)
-    int.se[i]<-(log.H-log.H.upper)/qnorm(.975)
+    int.se[i]<-(log.H-log.H.upper)/stats::qnorm(.975)
 
     p.val<-p[,i]
     quants<-quantile(p.val,(1:(g-1))/g)
@@ -173,9 +186,6 @@ val.surv.mi<-function(p,y,g=5,main="",save_plots=FALSE,time=NULL,lim=c(0,1),dist
     for (j in 1:g){
       sub<-(cuts==levels(cuts)[j])
       if (sum(y[sub,2])>0){
-        # sf<-survival::survfit(y~1,subset=sub)
-        # y.groups[i,j,1]<-utils::tail(sf$cumhaz,1)
-        # y.groups[i,j,2]<-utils::tail(sf$std.chaz,1)} else {y.groups[i,j,]<- -Inf}
         sf<-survival::survfit(y~1,subset=sub,conf.type="log-log")
         y.groups[i,j,1]<-log(-log(utils::tail(sf$surv,1)))
         y.groups[i,j,2]<-(log(-log(utils::tail(sf$surv,1)))-log(-log(utils::tail(sf$upper,1))))/stats::qnorm(.975)} else {y.groups[i,j,]<- -Inf}
@@ -205,14 +215,18 @@ val.surv.mi<-function(p,y,g=5,main="",save_plots=FALSE,time=NULL,lim=c(0,1),dist
     obs.sm.mi.upper[j]<-1-exp(-exp(RC$est+stats::qnorm(.975)*RC$se))
   }
 
-  if (save_plots) grDevices::dev.new(grDevices::windows.options(width = 5, height = 5))
+  if (save_plots){
+    grDevices::png(file=file_path)
+  }
   graphics::par(mar = c(5,5,2,1))
 
   graphics::par(mar = c(5,5,2,1))
   plot(lim,lim,type='l',xlab="Predicted probability",ylab="Observed frequency",main=main,lwd=1,bty='n')
 
-  graphics::polygon(x=c(p.sm.mi,rev(p.sm.mi)),y=c(obs.sm.mi.lower,rev(obs.sm.mi.upper)),border = NA,col="Lightgray")
-  graphics::lines(p.sm.mi,obs.sm.mi,lwd=2,col="Darkgray")
+  if (CI){
+    graphics::polygon(x=c(p.sm.mi,rev(p.sm.mi)),y=c(obs.sm.mi.lower,rev(obs.sm.mi.upper)),border = NA,col="Lightgray")
+    graphics::lines(p.sm.mi,obs.sm.mi,lwd=2,col="Darkgray")
+  }
 
   graphics::lines(lim,lim)
   graphics::abline(v=quants,col="darkgrey",lwd=1,lty=2)
@@ -237,10 +251,9 @@ val.surv.mi<-function(p,y,g=5,main="",save_plots=FALSE,time=NULL,lim=c(0,1),dist
     f0	<-f0[j0]
     maxf <-max(f0)
     f0	<-(0.1*f0)/maxf
-    #graphics::segments(bins0,line.bins,bins0,length.seg*-f0+line.bins)
     graphics::segments(bins0,line.bins,bins0,length.seg*f0+line.bins)
   }
-  # combine intercept, slope, and cindex
+
   int.mi<-Rubin.combine(int,int.se)
   slope.mi<-Rubin.combine(slope,slope.se)
   cindex.mi<-Rubin.combine(cindex,cindex.se)
@@ -251,11 +264,9 @@ val.surv.mi<-function(p,y,g=5,main="",save_plots=FALSE,time=NULL,lim=c(0,1),dist
                            paste("c =",format(round(cindex.mi$est,2),nsmall=2))),
          box.col="white",  bg = "white",cex=1)
 
-
-
-
-  # save plots
-  if (save_plots) {grDevices::savePlot(main,type="emf"); grDevices::dev.off()}
+  if (save_plots){
+    grDevices::dev.off()
+  }
 
   return(list(main=main,
               n=n,quants=quants,
