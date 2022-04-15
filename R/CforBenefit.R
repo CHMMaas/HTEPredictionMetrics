b567c2edfcf9bc0fd1f71737019bed94fede876e
Carolien Maas <c.h.m.maas@erasmusmc.nl>
2022-04-15 18:15:36 +0200

    Description

--- R/CforBenefit.R
+++ R/CforBenefit.R
@@ -11,9 +11,9 @@
 #' @importFrom stats quantile
 #'
 #' @param Y a vector of outcomes
-#' @param W a vector of treatment assignment; 1 for active treatment; 0 for control
+#' @param W a vector of treatment assignment; 1 for active treatment; 0 for control (or alternative treatment), note: make sure that W consists only of values of 0 and 1
 #' @param X a matrix of patient characteristics or individualized treatment effect predictions
-#' @param p.0 a vector of outcome probabilities under control
+#' @param p.0 a vector of outcome probabilities under control (or alternative treatment)
 #' @param p.1 a vector of outcome probabilities under active treatment
 #' @param tau.hat a vector of individualized treatment effect predictions
 #' @param CI boolean; TRUE compute confidence interval; default=FALSE do not compute confidence interval (default=FALSE)
