3c7c95fdc4e8f5627360eac7213bc2d141fba666
Carolien Maas <c.h.m.maas@erasmusmc.nl>
2022-12-29 10:13:27 +0100

    Check user input of X

--- R/matching.R
+++ R/matching.R
@@ -72,6 +72,7 @@ match.patients <- function(Y, W, X,
 
   stopifnot("W must be a vector" = is.vector(W))
   stopifnot("W must only consists of zeros and ones" = !sum(sort(unique(W))-c(0, 1)))
+  stopifnot("X must be a vector or matrix, use as.matrix(X) instead" = is.vector(X) | is.matri(X))
   stopifnot("p.0 must be a vector" = is.vector(p.0))
   stopifnot("p.1 must be a vector" = is.vector(p.1))
   stopifnot("tau.hat must be a vector" = is.vector(tau.hat))
